759af8487d4f6c146ef3d1eb1959ac0cd5b4f617
Jan K <jan.kaiponen@gmail.com>
2019-02-01 08:25:57 +0200

    Assingment

---
+++ Section-5-expressjs/Assignment/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+
+
+app.use('/users', (req, res, next) => {
+    console.log("users")
+    res.send('<h1>Users</h1>')
+})
+
+
+/* app.use ('/', (req, res, next) => {
+     console.log('Middleware I')
+     next()
+}) */
+
+app.use ('/', (req, res, err) => {
+    console.log('Middleware I')
+    res.send('<h1>Middleware II')
+})
+
+
+app.listen(3000)
+
+
+
+
+
--- Section-5-expressjs/app.js
+++ Section-5-expressjs/app.js
@@ -4,17 +4,27 @@ const express = require('express')
 
 const app = express()
 
-app.use( (req, res, next) => { 
-    console.log("In the Middle")
-    next()
+
+
+app.use('/products', (req, res, next) => { 
+    console.log("In the Middle II")
+    res.write('<h1>HELLO PRODUCTS</h1>')
+    res.send()
+    
 })
 
-app.use( (req, res, next) => { 
+
+
+
+app.use('/', (req, res, next) => { 
     console.log("In the Middle II")
     res.write('<h1>HELLO</h1>')
     res.send()
     
 })
+
+
+
 
 const server = http.createServer(app)
 
