189e557aceeaa0989accdd94601ff5f5df1d1f7f
Jan K <jan.kaiponen@gmail.com>
2019-02-02 15:46:04 +0200

    Convert HMTL pages to pug pages and layouts

---
+++ Section-6-dynamics-content/starter.1/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const admin = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.set('view engine', 'pug')
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', admin.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).render('404')
+});
+
+app.listen(3000);
