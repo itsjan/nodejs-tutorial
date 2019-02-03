e2598bcb8d90d9fa348031e245b7bb8b02b16ceb
Jan K <jan.kaiponen@gmail.com>
2019-02-03 16:14:14 +0200

    Project to use ejs as the view engine 🙄

---
+++ Section-6-dynamics-content/starter.ejs/routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  console.table(adminData.products)
+  //res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {prods: adminData.products, pageTitle: 'My Shop', path:'/'})
+});
+
+module.exports = router;
