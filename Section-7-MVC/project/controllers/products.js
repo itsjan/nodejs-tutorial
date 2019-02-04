c6f938ef543222d2bf291500a9ce72f452cb2a8d
Jan K <jan.kaiponen@gmail.com>
2019-02-04 14:50:08 +0200

    Add file storage for products (model)

--- Section-7-MVC/project/controllers/products.js
+++ Section-7-MVC/project/controllers/products.js
@@ -1,33 +1,37 @@
 const Product = require('../models/product')
 
 exports.getProductsPage = (req, res, next) => {
-    console.table(Product.fetchAll())
+    //console.table(Product.fetchAll())
 
-    const products = Product.fetchAll()
+    const products = Product.fetchAll((products) => {
+        console.log('GET PRODUCTS PAGE:')
+        console.table(products)
 
-    res.render('shop', {
-      prods: products,
-      pageTitle: 'Shop',
-      path: '/',
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCSS: true
-    });
-  }
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length,
+            activeShop: true,
+            productCSS: true
+        });
+    })
+
+}
 
 exports.postAddProduct = (req, res, next) => {
     const newProduct = new Product(req.body.title)
     newProduct.save()
     //   products.push({ title: req.body.title });
     res.redirect('/');
-  }
+}
 
 exports.getAddProductPage = (req, res, next) => {
-  res.render('add-product', {
-    pageTitle: 'Add Product',
-    path: '/admin/add-product',
-    formsCSS: true,
-    productCSS: true,
-    activeAddProduct: true
-  });
+    res.render('add-product', {
+        pageTitle: 'Add Product',
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    });
 }
--- Section-7-MVC/project/models/product.js
+++ Section-7-MVC/project/models/product.js
@@ -1,17 +1,43 @@
-const products = []
-
+const fs = require('fs')
+const path = require('path')
+const rootDir = require('../util/path')
 
 module.exports = class Product {
 
-    constructor(title){
-        this.title = title     
+    constructor(title) {
+        this.title = title
     }
 
-    save(){
-        products.push(this)
+    save() {
+        const p = path.join(
+            rootDir,
+            'data',
+            'products.json')
+
+        fs.readFile(p, (err, fileContents) => {
+            let products = []
+
+            if (!err) {
+                products = JSON.parse(fileContents)
+            }
+            products.push(this)
+            fs.writeFile(p, JSON.stringify(products), (err) => console.log(err))
+        })
     }
 
-    static fetchAll() {
-        return products
+    static fetchAll(cb) {
+        const p = path.join(
+            rootDir,
+            'data',
+            'products.json')
+
+          fs.readFile(p, (err, fileContents) => {
+            
+            if (!err) {
+                cb (JSON.parse(fileContents) )
+            }
+            else cb ( [] )
+        
+        })
     }
 }
