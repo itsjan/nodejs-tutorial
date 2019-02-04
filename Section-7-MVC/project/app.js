38e82cf146e92d9a99096e9d21ad113c8b4f709f
Jan K <jan.kaiponen@gmail.com>
2019-02-04 12:56:03 +0200

    Add error controller

--- Section-7-MVC/project/app.js
+++ Section-7-MVC/project/app.js
@@ -1,24 +1,26 @@
-const path = require('path');
+const path = require('path')
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const bodyParser = require('body-parser')
 
-const app = express();
+const errorController = require('./controllers/error')
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+const app = express()
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set('view engine', 'ejs')
+app.set('views', 'views')
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
 
-app.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoutes)
+app.use(shopRoutes)
+app.use(errorController.get404Page)
+
+
+app.listen(3000)
